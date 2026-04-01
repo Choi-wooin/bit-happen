b4557c48b9f8b9e815bb61a0630ede9119a23ae7
Choi Wooin <87743305+Choi-wooin@users.noreply.github.com>
2026-04-01 10:24:08 +0900

    http 접속 에디터에서 iframe(유튜브)  미리보기 안되는 현상

--- assets/js/app-version.js
+++ assets/js/app-version.js
@@ -1,12 +1,1 @@
 window.BIT_HAPPENS_ASSET_VERSION = '20260401a';
-
-// Redirect HTTP → HTTPS on production (skip localhost for dev)
-(function () {
-  if (
-    location.protocol === 'http:' &&
-    location.hostname !== 'localhost' &&
-    location.hostname !== '127.0.0.1'
-  ) {
-    location.replace('https://' + location.host + location.pathname + location.search + location.hash);
-  }
-})();
