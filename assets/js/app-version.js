126cc39958853be5043101b7b5af43a5e2249040
Choi Wooin <87743305+Choi-wooin@users.noreply.github.com>
2026-05-03 19:16:08 +0900

    favicon 관리자 아이콘 변경

--- assets/js/app-version.js
+++ assets/js/app-version.js
@@ -1,2 +1,2 @@
-window.BIT_HAPPENS_ASSET_VERSION = '20260503-004';
+window.BIT_HAPPENS_ASSET_VERSION = '20260503-005';
 console.log('%c[BIT HAPPENS] asset version: ' + window.BIT_HAPPENS_ASSET_VERSION, 'color:#1473e6;font-weight:bold');
