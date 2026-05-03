81d526bc0d61b050cbf4e339fc7d5e05c05e8a75
Choi Wooin <87743305+Choi-wooin@users.noreply.github.com>
2026-05-03 19:19:18 +0900

    favicon B로 변경

--- assets/js/app-version.js
+++ assets/js/app-version.js
@@ -1,2 +1,2 @@
-window.BIT_HAPPENS_ASSET_VERSION = '20260503-005';
+window.BIT_HAPPENS_ASSET_VERSION = '20260503-006';
 console.log('%c[BIT HAPPENS] asset version: ' + window.BIT_HAPPENS_ASSET_VERSION, 'color:#1473e6;font-weight:bold');
