cd863a2289bf1a7b7bb293ade7436fc6e8d61226
Choi Wooin <87743305+Choi-wooin@users.noreply.github.com>
2026-05-03 19:11:36 +0900

    favicon 추가2

--- assets/js/app-version.js
+++ assets/js/app-version.js
@@ -1,2 +1,2 @@
-window.BIT_HAPPENS_ASSET_VERSION = '20260503-003';
+window.BIT_HAPPENS_ASSET_VERSION = '20260503-004';
 console.log('%c[BIT HAPPENS] asset version: ' + window.BIT_HAPPENS_ASSET_VERSION, 'color:#1473e6;font-weight:bold');
