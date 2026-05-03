aabab6f3a5b4228497c0b7597a8916f8cd9d954d
Choi Wooin <87743305+Choi-wooin@users.noreply.github.com>
2026-05-03 17:21:00 +0900

    20260503-001

--- assets/js/app-version.js
+++ assets/js/app-version.js
@@ -1,1 +1,2 @@
-window.BIT_HAPPENS_ASSET_VERSION = '20260503a';
+window.BIT_HAPPENS_ASSET_VERSION = '20260503-001';
+console.log('%c[BIT HAPPENS] asset version: ' + window.BIT_HAPPENS_ASSET_VERSION, 'color:#1473e6;font-weight:bold');
